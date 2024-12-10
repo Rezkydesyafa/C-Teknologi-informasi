96bd106427e53e849bf202c3b67d68249531bf7d
Rezky Desyafa <rezkydesyafa@gmail.com>
2024-12-10 10:31:29 +0700

    update tugas stlib

--- Tugas Kelompok/stlib.h/tempCodeRunnerFile.c
+++ Tugas Kelompok/stlib.h/tempCodeRunnerFile.c
@@ -6,31 +6,31 @@ int main()
 {
     int angkaRahasia, tebakan, peluang = 5;
 
-    srand(time(0));                  // Inisialisasi seed untuk angka acak
-    angkaRahasia = rand() % 100 + 1; // Angka acak antara 1-100
+    srand(time(0)); // Inisialisasi seed untuk angka acak
+    angkaRahasia = rand() % 100 + 1;
 
-    printf("Tebak angka antara 1-100! Anda memiliki %d peluang.\n", peluang);
+    printf("Tebak angka antara 1-100 | Anda memiliki %d peluang.\n", peluang);
 
     for (int i = 0; i < peluang; i++)
     {
+        printf("-------------------------------------\n");
         printf("Masukkan tebakan Anda: ");
         scanf("%d", &tebakan);
 
         if (tebakan == angkaRahasia)
         {
+            printf("-------------------------------------\n");
             printf("Selamat! Anda menebak dengan benar.\n");
+            printf("-------------------------------------\n");
             return 0;
         }
         else if (tebakan < angkaRahasia)
         {
+            printf("-------------------------------------\n");
             printf("Terlalu kecil!\n");
         }
         else
         {
+            printf("-------------------------------------\n");
             printf("Terlalu besar!\n");
         }
-    }
-
-    printf("Maaf, Anda kehabisan peluang. Angka yang benar adalah %d.\n", angkaRahasia);
-    return 0;
-}
